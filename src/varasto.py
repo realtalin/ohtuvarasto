569ea3d7aeb177dd95989e481a059d5cd94d1166
Sampo <57003465+realtalin@users.noreply.github.com>
2022-11-14 20:02:41 +0200

    Tee tehtävä 3 ja riko pylint

--- src/varasto.py
+++ src/varasto.py
@@ -1,10 +1,6 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = max(0, tilavuus)
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
@@ -16,7 +12,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa riko pylint riko pylint riko pylint riko pylint riko pylint riko pylint riko pylint riko pylint 
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
